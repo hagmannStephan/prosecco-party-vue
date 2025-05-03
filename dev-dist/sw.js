a9760074e061d970043b892283cb034bd0ff3cf8
Hagmann Stephan <stephanhagmann@outlook.com>
2025-05-03 15:46:52 +0200

    chore: added v1 of manifest

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-54d0af47'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.1qrma9aaaf8"
+    "revision": "0.2qu3b9h0k"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
--- vite.config.ts
+++ vite.config.ts
@@ -11,7 +11,38 @@ export default defineConfig({
       registerType: 'autoUpdate',
       devOptions: {
         enabled: true
-      } 
+      },
+      manifest: {
+        'name': 'Name of the Game',
+        'short_name': 'Party Games',
+        'description': 'A collection of party games for you and your friends.',
+        'theme_color': '#811e73',
+        'background_color': '#300056',
+        'display': 'standalone',
+        'orientation': 'portrait',
+        'start_url': '/',
+        // TODO: Add icons
+      },
+      workbox: {
+        // Use the default Workbox options for caching and offline support
+        globPatterns: [
+          // Your app's main assets
+          'index.html',
+          'favicon.ico',
+          'manifest.webmanifest',
+          
+          // Your specific static files
+          'icons/plus-1.svg',
+          'icons/refresh.svg',
+          'font/Noto_Sans/NotoSans-SemiBold.ttf',
+          
+          // JavaScript and CSS (including your style.css which gets processed)
+          '**/*.js',
+          '**/*.css'
+        ],
+        // Ensure navigations work offline
+        navigateFallback: 'index.html'
+      }
     })
   ],
   resolve: {
