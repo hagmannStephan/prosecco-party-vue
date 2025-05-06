a0163e20f92f5ebe744358177fb95a3d23891273
Hagmann Stephan <stephanhagmann@outlook.com>
2025-05-06 08:18:04 +0200

    feat: updated game-config to work with groups

--- src/locales/de/games/activity/ActivityConfig.ts
+++ src/locales/de/games/activity/ActivityConfig.ts
@@ -7,14 +7,22 @@ export default {
             "1": "Stephan de Leuw",
             "2": "Liam de Löli",
             "3+": "Jojo de Chef"
-        }
+        },
+        "add": "Spieler:in hinzufügen",
+    },
+    "groups": {
+        "num": "Anzahl Gruppen",
+        "setup": "Gruppenkonfiguration",
+        "name": "Gruppennamen",
+        "name-placeholder": "S Leuwe Rudel",
+        "default": "Gruppe {num}",
     },
     "round": {
         "num": "Anzahl Runden",
         "time": "Zeit pro Runde (Sekunden)",
         "sec": "Sekunden"
     },
-    "mode": {
+    "difficulty": {
         "title": "Schwierigkeit",
         "easy": "Einfach",
         "normal": "Normal",
@@ -24,6 +32,9 @@ export default {
         "min-players": "Es müssen mindestens 2 Spielende teilnehmen.",
         "name-required": "Bitte geben Sie einen Namen für alle Spielenden ein.",
         "mode-required": "Bitte wählen Sie mindestens einen Spielmodus.",
+        "min-groups": "Es müssen mindestens 2 Gruppen gebildet werden.",
+        "min-players-per-group": "Es muss mindestens 1 Spieler pro Gruppe geben.",
+        "group-name-required": "Bitte geben Sie einen Namen für alle Gruppen ein.",
     },
     "start-game": "Spiel starten!",
 }
--- src/locales/en/games/activity/ActivityConfig.ts
+++ src/locales/en/games/activity/ActivityConfig.ts
@@ -1,5 +1,5 @@
 export default {
-    "title": "Game Configuration",
+    "title": "Configuration",
     "player": {
         "num": "Number of Players",
         "name": "Names of Players",
@@ -7,14 +7,22 @@ export default {
             "1": "Stephan de Leuw",
             "2": "Liam de Löli",
             "3+": "Jojo de Chef"
-        }
+        },
+        "add": "Add Player",
+    },
+    "groups": {
+        "num": "Number of Groups",
+        "setup": "Group Configuration",
+        "name": "Group Names",
+        "name-placeholder": "S Leuwe Pack",
+        "default": "Group {num}",
     },
     "round": {
         "num": "Number of Rounds",
         "time": "Time per Round (Seconds)",
         "sec": "Seconds"
     },
-    "mode": {
+    "difficulty": {
         "title": "Difficulty",
         "easy": "Easy",
         "normal": "Normal",
@@ -24,6 +32,9 @@ export default {
         "min-players": "At least 2 players must participate.",
         "name-required": "Please enter a name for all players.",
         "mode-required": "Please select at least one game mode.",
+        "min-groups": "At least 2 groups must be formed.",
+        "min-players-per-group": "There must be at least 1 player per group.",
+        "group-name-required": "Please enter a name for all groups.",
     },
     "start-game": "Start Game!",
 }
