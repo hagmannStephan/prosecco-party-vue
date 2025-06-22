268b811c2bf15fae0a49a076d8fdc10f630caeea
Hagmann Stephan <stephanhagmann@outlook.com>
2025-06-22 20:30:42 +0200

    chore: added file structure for penaltiesStore

---
+++ src/stores/__tests__/penaltiesStore.test.ts
@@ -0,0 +1,8 @@
+import { describe, it, expect } from 'vitest'
+
+describe('Penalties Store', () => {
+    it('TODO: Write tests for penaltiesStore', () => {
+        // Fail until tests are written
+        expect(1).toBe(2);
+    });
+});
